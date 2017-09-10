b2e2bc49eddf2ae232c00427a66b9b5bf8758064
Anson Ho <mrho888@gmail.com>
2017-09-11 01:05:37 +0800

    Clean

--- rank.py
+++ rank.py
@@ -2,14 +2,16 @@
 
 import json
 import urllib2
+
 from unidecode import unidecode
+
 
 CONTEST_CODE = raw_input('Please enter a contest code (e.g. agc017): ')
 COUNTRY_CODE = raw_input('Please enter a country code (e.g. BY, HK): ')
-
 LINK = 'http://' + CONTEST_CODE + '.contest.atcoder.jp/standings'
-
 HTML = urllib2.urlopen(urllib2.Request(LINK)).read().split('\n')
+
+
 for i in range(0, len(HTML)):
     if 'ATCODER.standings' in HTML[i]:
         start = HTML[i + 2].index('[')
@@ -23,8 +25,11 @@ for i in range(0, len(HTML)):
                 for k in j['tasks']:
                     if 'score' in k and k['score'] > 0:
                         solved += 1
-                data.append((str(j['rank']), unidecode(j['user_name']), j['user_screen_name'], \
-				             str(j['rating']), '%d / %d' % (solved, len(j['tasks']))))
+                data.append((str(j['rank']),
+                             unidecode(j['user_name']),
+                             j['user_screen_name'],
+                             str(j['rating']),
+                             '%d / %d' % (solved, len(j['tasks']))))
         for j in data:
             for k in range(5):
                 leng[k] = max(leng[k], len(j[k]) + 1)
